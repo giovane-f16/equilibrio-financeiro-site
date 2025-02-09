efb1dc2565687947cd11b8b89c1db2c507749ff0
giovanef16 <giovane_16silva@hotmail.com>
2025-02-09 13:25:40 -0300

    Incluindo suporte para blocos

--- wp-content/themes/equilibrio-financeiro-tema/functions.php
+++ wp-content/themes/equilibrio-financeiro-tema/functions.php
@@ -4,4 +4,9 @@ add_filter("show_admin_bar", "__return_false");
 
 add_action("after_setup_theme", function () {
     add_theme_support("post-thumbnails");
+    add_theme_support("wp-block-styles"); // Suporte aos estilos de blocos
+    add_theme_support("align-wide"); // Suporte para alinhamento amplo
+    add_theme_support("editor-styles"); // Permite estilos no editor Gutenberg
+    add_theme_support("responsive-embeds"); // Suporte para embeds responsivos
+    add_theme_support("custom-spacing"); // Permite espaçamentos personalizados
 });
